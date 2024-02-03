ac5a44e831fadb0ea020c97f8887bf4e196e1030
frankwillian <frank19valentim@gmail.com>
2024-02-03 16:47:04 -0300

    Test

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,14 +1,24 @@
+const crypto = require('crypto');
 const axios = require('axios');
 const { response } = require('express');
+const postsService = require('../service/postsService');
+const { func } = require('../infra/database');
+
+const generate = function () {
+    return crypto.randomBytes(20).toStringJ('hex');
+}
 
 test ('Should get posts', async function () {
-    await axios({
+    // Given - dado que
+    const post1 = postsService({ title: generate(), content: generate() });
+    const post2 = postsService({ title: generate(), content: generate() });
+    const post3 = postsService({ title: generate(), content: generate() });
+    // When - quando acontecer 
+    const reponse = await axios({
         url: 'http://localhost:3000',
         method: 'get'
     });
-    const posts = response.data;
+    const posts = response.data
+    // Then - então
     expect(posts).toHaveLength(3);
-    const [firstPost] = posts;
-    expect(firstPost.id).toBe(1);
-    expect(firstPost.title).toBe('REST API: Métodos');
 });
